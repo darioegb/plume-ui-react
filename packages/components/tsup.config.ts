b7d2cb70cfdb717777c03231ae926c5f4d56fd71
Dario Gonzalez <darioegb@gmail.com>
2023-10-24 18:18:09 -0300

    fix: rollback creation tsup esbuild plugin for css modules

--- packages/components/tsup.config.ts
+++ packages/components/tsup.config.ts
@@ -1,6 +1,9 @@
 // tsup.config.ts
+import { promises as fsPromises } from 'node:fs'
+import path from 'node:path'
 import { defineConfig } from 'tsup'
-import { cssModule } from 'esbuild-css-modules'
+import postcss from 'postcss'
+import postcssModules from 'postcss-modules'
 
 export default defineConfig((options) => ({
   entry: ['src/index.ts'],
@@ -14,6 +17,44 @@ export default defineConfig((options) => ({
   tsconfig: 'tsconfig.json',
   external: ['react'],
   esbuildPlugins: [
-    cssModule
+    {
+      name: 'css-module',
+      setup(build): void {
+        build.onResolve({ filter: /\.module\.css$/, namespace: 'file' }, (args) => ({
+          path: `${args.path}#css-module`,
+          namespace: 'css-module',
+          pluginData: {
+            pathDir: path.join(args.resolveDir, args.path),
+          },
+        }))
+        build.onLoad({ filter: /#css-module$/, namespace: 'css-module' }, async (args) => {
+          const { pluginData } = args as {
+            pluginData: { pathDir: string }
+          }
+          const source = await fsPromises.readFile(pluginData.pathDir, 'utf8')
+          let cssModule = {}
+          const result = await postcss([
+            postcssModules({
+              getJSON(_, json) {
+                cssModule = json
+              },
+            }),
+          ]).process(source, { from: pluginData.pathDir })
+          return {
+            pluginData: { css: result.css },
+            contents: `import "${pluginData.pathDir}"; export default ${JSON.stringify(cssModule)}`,
+          }
+        })
+        build.onResolve({ filter: /\.module\.css$/, namespace: 'css-module' }, (args) => ({
+          path: path.join(args.resolveDir, args.path, '#css-module-data'),
+          namespace: 'css-module',
+          pluginData: args.pluginData as { css: string },
+        }))
+        build.onLoad({ filter: /#css-module-data$/, namespace: 'css-module' }, (args) => ({
+          contents: (args.pluginData as { css: string }).css,
+          loader: 'css',
+        }))
+      },
+    },
   ],
 }))
